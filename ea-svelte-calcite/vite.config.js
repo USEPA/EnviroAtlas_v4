ee73ad814418378a91af32b21c25a5cc16f70cea
aevans04 <evans.aaron@epa.gov>
2023-08-18 12:29:38 -0400

    Changed vite config to allow base to be "ea" app can be hosted alongside legacy app. Also add public dir so that static stuff moved to dist folder.  also added src to be resolved to src directory but vite seems to be already doing this when using /src/<whatever>.

--- ea-svelte-calcite/vite.config.js
+++ ea-svelte-calcite/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+import path from 'path';
+const projectRootDir = path.resolve(__dirname);
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'ea',
+  publicDir: 'www',
+  resolve: {
+    alias:[
+        {
+            find: 'src',
+            replacement: path.resolve(projectRootDir, 'src')
+        }
+    ]
+  },
   plugins: [svelte()],
 })
