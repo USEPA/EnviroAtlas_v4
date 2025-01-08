0ba3cbe088e79ec385d9a32cdab1ea7460c3dd2b
Thera Lombardi <lombardi.thera@epa.gov>
2025-01-08 13:45:33 -0500

    Fix styling of list items.

--- client/src/components/DataCatalog/DataList.svelte
+++ client/src/components/DataCatalog/DataList.svelte
@@ -56,7 +56,7 @@
         // Create for loop to load in all subtopics to build UI object
         for (const prop in data) {
             // console.log(`${prop}: ${data[prop].topic}`);
-            //apply topic to subtopic params
+            // apply topic to subtopic params
             let subtopicParams = {
                 select: encodeURIComponent(`{"topic":0,"categoryTab":0,"layers":{"layerID":1,"subLayerName":1,"description":1,"tags":1,"name":1}}`),
                 where: encodeURIComponent(`{"topic":"${data[prop].topic}"}`)
@@ -64,16 +64,13 @@
             // get subtopic object from api
             // return promise object resolve, not the whole promise object
             let res = await getEaData("/ea/api/subtopics", subtopicParams);
-            // console.log(res);
-            // take the result and put into data, subtopic object
-            data[prop].subtopic = res;
+            // take the result and put into store subtopic object
             $nationalItems[prop].subtopic = res;
         }
-        // console.log(data);
-        //$catalog.national = data;
         return data
     }
 
+    // wait for eaTopics to finish before updating data for catalog UI
     eaTopics.then((result) => getEaSubtopics(result));
 
     async function updateListStyle(elem) {
@@ -87,8 +84,10 @@
         shadow.adoptedStyleSheets = [stylesheet];
     }
 
-    //TODO: need to fix this since the api calls changed the call stack 
-    (async () => {
+    // Need to wait for eaTopics to load before styling list
+    eaTopics.then(() => styleList());
+
+    async function styleList() {
         await customElements
         .whenDefined("calcite-list-item");
             // TODO: tidy this up.
@@ -108,7 +107,7 @@
             listBNF.forEach((elem) => {
                 updateListStyle(elem);
             });
-    })();
+    };
 
     const handleFabClick = () => {
         let bar = document.getElementById("left-action-bar");
