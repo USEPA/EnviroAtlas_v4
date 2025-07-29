2eaab86954200dd3a6ec23d809fb0efc6580b85b
tlomba01 <lombardi.thera@epa.gov>
2025-07-29 14:15:12 -0400

    Update order of domains in dropdown list.

--- client/src/components/ClimateChangeViewer/Bookmark.svelte
+++ client/src/components/ClimateChangeViewer/Bookmark.svelte
@@ -7,11 +7,11 @@
 
     let bookmarks = [
         { name: "CONUS", text: "Continental US", selected: true, extentObj: {xmax: -5347672, xmin: -15914327, ymax: 7733573, ymin: 1853426}}, 
-        { name: "Hawaii", text: "Hawaii", extentObj: { xmax: -17232996.3537, xmin: -17838620.5975, ymax: 2539458.2165, ymin: 2144428.1256}},
         { name: "Alaska", text: "Alaska", extentObj: { xmax: -15876210.0, xmin: -19061453.32, ymax: 12511315.0, ymin: 6923265.0}},
+        { name: "Hawaii", text: "Hawaii", extentObj: { xmax: -17232996.3537, xmin: -17838620.5975, ymax: 2539458.2165, ymin: 2144428.1256}},
+        { name: "PacIsles", text: "Pacific Islands", extentObj: { xmax: -18844510.286599636, xmin: -19163997.22517633, ymax: -1444391.585028562, ymin: -1774134.0808157807}},
         { name: "Guam", text: "Guam", extentObj: { xmax: 16446175.757969558, xmin: 16004237.379520258, ymax: 2301068.6854367177, ymin: 1503028.389935526}},
         { name: "Puerto Rico,Virgin Islands", text: "Puerto Rico & Virgin Islands", extentObj: { xmax: -7187363.955800, xmin: -7564321.223000, ymax: 2098168.552400, ymin: 1999395.310000}},
-        { name: "PacIsles", text: "Pacific Islands", extentObj: { xmax: -18844510.286599636, xmin: -19163997.22517633, ymax: -1444391.585028562, ymin: -1774134.0808157807}}
     ];
 
     const onGeogChange = (bookmark) => {
