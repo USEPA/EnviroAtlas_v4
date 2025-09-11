63461d5ad5a03ea8d30da7a3c6885a48e7429244
tlomba01 <lombardi.thera@epa.gov>
2025-09-11 16:26:42 -0400

    Forgot to add climate component.

--- client/src/components/ClimateChangeViewer/ClimateChangeViewer.svelte
+++ client/src/components/ClimateChangeViewer/ClimateChangeViewer.svelte
@@ -14,9 +14,11 @@
     import "@esri/calcite-components/dist/components/calcite-action";
     import "@esri/calcite-components/dist/components/calcite-action-group";
     import "@esri/calcite-components/dist/components/calcite-tooltip";
+    import { hasValueUndefined } from "src/shared/utilities.js";
     
     export let geography;
 
+    let climRefs = [];
     let options = [ 
         { name: "Variable", options: [ 
             {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "PRin", label: "Change in Precipitation (in)"},  
@@ -26,12 +28,20 @@
             {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "mxTF", label: "Change in Maximum Temperature (°F)"},
             {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "miTF", label: "Change in Minimum Temperature (°F)"}
         ]}, 
+        { name: "Scenario", options: [
+            {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands", value: "ssp126", label: "SSP1-2.6 (>3.6°F by 2100)"},
+            {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "ssp245", label: "SSP2-4.5 (4.9 ± 1.6 °F by 2100)"},
+            {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands", value: "ssp370", label: "SSP3-7.0 (6.5 ± 1.3 °F by 2100)"},
+            {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "ssp585", label: "SSP5-8.5 (7.9 ± 2.3 °F by 2100)"},
+            {domains: "CONUS", value: "rcp45", label: "RCP-4.5 (Peak Emissions Year 2040"},
+            {domains: "CONUS", value: "rcp85", label: "RCP-8.5 (Peak Emissions After 2100"}
+        ]},
         { name: "Season", options: [ 
             {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "Spring", label: "Spring"},  
             {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "Summer", label: "Summer"},
             {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "Fall", label: "Fall"},
             {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "Winter", label: "Winter"},
-            {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "Annual", label: "Annual"}
+            {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "A", label: "Annual"}
         ]}, 
         { name: "Period", options: [ 
             {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "Near Term", label: "Near Term"},  
@@ -40,20 +50,40 @@
             {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "XXI 2025-2054 to 2045-2074", label: "XXI 2025-2054 to 2045-2074"},
             {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "XXI 2025-2054 to 2070-2100", label: "XXI 2025-2054 to 2070-2100"}
         ]}, 
-        { name: "Scenario", options: [
-            {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands", value: "ssp126", label: "SSP1-2.6 (>3.6°F by 2100)"},
-            {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "ssp245", label: "SSP2-4.5 (4.9 ± 1.6 °F by 2100)"},
-            {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands", value: "ssp370", label: "SSP3-7.0 (6.5 ± 1.3 °F by 2100)"},
-            {domains: "Alaska,AmericanSamoa,Guam,Hawaii,Puerto Rico,Virgin Islands,CONUS", value: "ssp585", label: "SSP5-8.5 (7.9 ± 2.3 °F by 2100)"},
-            {domains: "CONUS", value: "rcp45", label: "RCP-4.5 (Peak Emissions Year 2040"},
-            {domains: "CONUS", value: "rcp85", label: "RCP-8.5 (Peak Emissions After 2100"}
-        ]}
     ];
 
     $: options_filtered = options.map(obj => {
         return {...obj, options: obj.options.filter(opt => opt.domains.includes(geography))}
     })
 
+    //Code to adapt from v3
+    function loadOCONUS() {
+        let selections = getSelections()
+    //     var domainText = this.domainOCONUS.options[this.domainOCONUS.selectedIndex].text;
+    //     var scenario = dojo.byId("modelSelectionOCONUS").value;
+    //     map.setExtent(this._zoomToOCONUSArea(domain));
+        var fieldname = _buildOconusField(selections);
+    };
+
+    function _buildOconusField(selections) {
+        // Need to build the field name from selections
+        //return ("ME" + dojo.byId("seasonSelectionOCONUS").value + dojo.byId("climateSelectionOCONUS").value + dojo.byId("periodSelectionOCONUS").value)
+    };
+
+    function getSelections() {
+        // Get selections as a dictionary
+        let selections = {}
+        climRefs.forEach(elem => {
+            let option = elem.placeholder
+            let value = elem.selectedItems[0]?.value
+            selections[option] = value
+        });
+        if (hasValueUndefined(selections)) {
+            console.log('give user notify')
+        } else {
+            return selections
+        }
+    }
 </script>
 
 <calcite-panel
@@ -65,17 +95,18 @@
     hidden
 >
     <calcite-block open heading="Climate">
-        {#each options_filtered as c (c.name)}
+        {#each options_filtered as clim, c (clim.name)}
         <div id="combobox-div">
             <calcite-combobox
+                bind:this={climRefs[c]}
                 id="climateVarSelect"
                 scale="m"
-                placeholder={c.name}
+                placeholder={clim.name}
                 selection-mode="single"
                 max-items="0"
                 overlay-positioning="absolute"
             >
-            {#each c.options as o}
+            {#each clim.options as o}
                 <calcite-combobox-item value={o.value} text-label={o.label}/>
             {/each}
             </calcite-combobox>
@@ -83,7 +114,7 @@
             ></calcite-button>
         </div>
         {/each}
-        <calcite-button>Add to map</calcite-button>
+        <calcite-button on:click={loadOCONUS}>Add to map</calcite-button>
     </calcite-block>
 </calcite-panel>
 
