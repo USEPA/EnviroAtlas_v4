9cb803953a1dcf2fc918981fd5a74d51a117b795
tlomba01 <lombardi.thera@epa.gov>
2025-10-07 16:56:17 -0400

    Finally got the calcite-chips working. Swap buttons for chips which look and behave decently out-of-the-box.

--- client/src/components/DataCatalog/CatalogActionBar.svelte
+++ client/src/components/DataCatalog/CatalogActionBar.svelte
@@ -47,47 +47,51 @@
     };
 
     /**
-     * The on:click function for category filter buttons. 
+     * The on:click function for category filter chips. 
      * Controls the categoryFilter store value for catalog filtering.
-     * Performs UI feedback of graying out the other selections.
      * Expands or collapses the topics in the list.
      * @param {object} cat - the selected category data dictionary
      */
     const onCatChange = (cat) => {
         if ($categoryFilter != cat.name) {
             $categoryFilter = cat.name;
-            catRefs.filter(catIcon => catIcon.classList.contains(cat.name))[0].classList.remove('filtered');
-            catRefs.filter(catIcon => !catIcon.classList.contains(cat.name)).forEach(elem => {
-                elem.classList.add('filtered')
-            });
             expandTopics();
         } else if ($categoryFilter === cat.name) {
             $categoryFilter = '';
-            catRefs.forEach(elem => {
-                elem.classList.remove('filtered')
-            });
             expandTopics(false);
         }
     };
 </script>
 
-{#if type=='national' || type=='subnational'}
+{#if type=='national'}
 <calcite-card>
 <calcite-action-bar id="catalog-search-filter" layout="horizontal" expand-disabled>    
     <calcite-input alignment='start' maxLength=20 type='text' scale='l' icon='search' bind:this={searchInput} on:calciteInputInput={()=>onSearch()} placeholder="Try searching"></calcite-input>
     <calcite-button id='count' scale='s'>{totalVisibleMaps} / {totalMapsCount} <br>
         Maps</calcite-button>   
 </calcite-action-bar>
-    <calcite-chip-group scale="s" slot="footer-start">
+    <calcite-chip-group 
+        scale='s' slot="footer-start" selection-mode="single" 
+        label="cat-filter-chip-group">
         {#each categories as cat, c (cat.name)}
-            {#if cat.name !== moreFilters && cat.name !== moreFilter2}
-                <calcite-button bind:this={catRefs[c]} alignment="start" class={cat.name} id="catFilter" round appearance="outline" on:click={()=>onCatChange(cat)}>
-                    <img alt={cat.name} style="width:20px;height:20px;background-color:{cat.color};border-radius:50%" src="https://enviroatlas.epa.gov/enviroatlas/interactivemap/widgets/SimpleSearchFilter/images/ES_Icons/{cat.icon}.png">
-                    {cat.label}
-                </calcite-button>    
-            {/if}
+        {#if cat.name !== moreFilters && cat.name !== moreFilter2}
+            <calcite-chip 
+            bind:this={catRefs[c]}
+                type="radio"
+                id='{cat.name}' 
+                scale='s' 
+                value={cat.name}
+                label={cat.name}
+                kind='brand' 
+                appearance='outline-fill'
+                on:calciteChipSelect={()=>onCatChange(cat)}
+            >
+                <img slot="image" alt={cat.name} style="width:20x;height:20px;background-color:{cat.color};border-radius:50%" src="https://enviroatlas.epa.gov/enviroatlas/interactivemap/widgets/SimpleSearchFilter/images/ES_Icons/{cat.icon}.png">{cat.label}
+        </calcite-chip>
+        {/if}
         {/each}
-        <calcite-button id="catFilter-popover-ref" icon-start="plus-circle" round appearance="outline">More Filters</calcite-button>
+        <calcite-chip id="catFilter-popover-ref" icon="plus-circle" round appearance="outline">More Filters</calcite-chip>
+        </calcite-chip-group> 
         <calcite-popover
             placement="trailing-start"
             overlay-positioning="fixed"
@@ -97,16 +101,20 @@
             closable
             heading="More Filters"
         >
-                    {#each categories as cat, c (cat.name)}
-                    {#if cat.name === moreFilters || cat.name === moreFilter2}
-                    <calcite-button bind:this={catRefs[c]} alignment="start" class={cat.name} id="catFilter" round appearance="outline" on:click={()=>onCatChange(cat)}>
-                        <img alt={cat.name} style="width:20px;height:20px;background-color:{cat.color};border-radius:50%" src="https://enviroatlas.epa.gov/enviroatlas/interactivemap/widgets/SimpleSearchFilter/images/ES_Icons/{cat.icon}.png">
+            <calcite-chip-group 
+                scale='s' selection-mode="single" 
+                label="cat-filter-chip-group">
+                {#each categories as cat, c (cat.name)}
+                {#if cat.name === moreFilters || cat.name === moreFilter2}
+                    <calcite-chip scale="s" bind:this={catRefs[c]} id="catFilter" round appearance="outline" on:click={()=>onCatChange(cat)}>
+                        <img slot="image" alt={cat.name} style="width:20px;height:20px;background-color:{cat.color};border-radius:50%" src="https://enviroatlas.epa.gov/enviroatlas/interactivemap/widgets/SimpleSearchFilter/images/ES_Icons/{cat.icon}.png">
                         {cat.label}
-                    </calcite-button>
-                    {/if}
-                    {/each}
+                    </calcite-chip>
+                {/if}
+                {/each}
+            </calcite-chip-group> 
         </calcite-popover>
-    </calcite-chip-group>
+    
  </calcite-card>
 {/if}
 
@@ -116,6 +124,10 @@
         margin: 3px;
     }
 
+    calcite-chip {
+        --calcite-chip-border-color: black
+    }
+    
     calcite-button#count {
         width:25%;
         margin: 3px;
@@ -125,22 +137,5 @@
         --calcite-button-background-color: white;
         --calcite-button-text-color: #005ea2;
         --calcite-button-border-color:#6a6a6a
-    }
-
-    calcite-button#catFilter {
-        --calcite-button-text-color: black;
-        --calcite-button-border-color: black;
-        --calcite-ui-focus-color: none !important;
-    }
-
-    calcite-button#catFilter-popover-ref {
-        --calcite-button-text-color: black;
-        --calcite-button-border-color: black;
-        --calcite-ui-focus-color: none !important;
-    }
-
-    calcite-button.filtered {
-        --calcite-button-background-color: #d3d3d3;
-        --calcite-ui-focus-color: none !important;
     }
 </style> 
