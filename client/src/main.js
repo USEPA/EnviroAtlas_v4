a6d0a9b6d691280dde7a825eb6fbcf464ff94002
tlomba01 <lombardi.thera@epa.gov>
2025-11-10 11:49:17 -0500

    Update packages with vulnerability alerts. Update to svelte 5.

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,13 @@
 import '/src/app.css';
 import App from '/src/App.svelte';
 import { setAssetPath } from '@esri/calcite-components/dist/components';
+import { mount } from "svelte";
+
 setAssetPath('https://js.arcgis.com/calcite-components/3.3.0/assets');
 
 if (!window.ea ) window.ea = {};
 
-const app = new App({
+const app = mount(App, {
   target: document.getElementById('app'),
 });
 
