ae404781029761f5cda332881e6f73900a692f57
tlomba01 <lombardi.thera@epa.gov>
2026-04-23 09:29:21 -0400

    Move basemaps icon in right side panel.

--- client/src/components/AppShell.svelte
+++ client/src/components/AppShell.svelte
@@ -339,11 +339,6 @@
       text="Add Data"
     ></calcite-action>
     <calcite-action
-      data-action-id="basemaps"
-      icon="basemap"
-      text="Basemaps"
-    ></calcite-action>
-    <calcite-action
       data-action-id="maptools"
       icon="system-management"
       text="Other Map Tools"
@@ -352,6 +347,11 @@
       data-action-id="sma-results"
       icon="mosaic-method-sum"
       text="Summarize My Area Results"
+    ></calcite-action>
+    <calcite-action
+      data-action-id="basemaps"
+      icon="basemap"
+      text="Basemaps"
     ></calcite-action>
   </calcite-action-bar>
   <calcite-panel
